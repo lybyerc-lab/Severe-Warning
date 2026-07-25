f7b51ea4e15feeb5654483fa7562905f9be78f63
lybyerc-lab <lybyerc@gmail.com>
2026-07-24 21:14:01 -0500

    Advance Build 5.2 identity

--- Assets/SevereWeather/Runtime/Core/BuildIdentity.cs
+++ Assets/SevereWeather/Runtime/Core/BuildIdentity.cs
@@ -6,8 +6,8 @@ namespace SevereWeather.Core
     public static class BuildIdentity
     {
         private static bool loaded;
-        private static string version = "0.1.8";
-        private static string build = "5.1";
+        private static string version = "0.1.9";
+        private static string build = "5.2";
         private static string commit = "local";
         private static string utc = "unknown";
 
