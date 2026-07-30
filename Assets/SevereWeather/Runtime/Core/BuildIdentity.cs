16fd428ea775bd075a3763a0f89b595e027f34ac
lybyerc-lab <lybyerc@gmail.com>
2026-07-29 21:42:00 -0500

    Identify Tornado Tactical v1 build

--- Assets/SevereWeather/Runtime/Core/BuildIdentity.cs
+++ Assets/SevereWeather/Runtime/Core/BuildIdentity.cs
@@ -6,8 +6,8 @@ namespace SevereWeather.Core
     public static class BuildIdentity
     {
         private static bool loaded;
-        private static string version = "0.1.9";
-        private static string build = "5.2";
+        private static string version = "0.2.0";
+        private static string build = "TTV1";
         private static string commit = "local";
         private static string utc = "unknown";
 
