3bdbf528fd5ed48448299e3a2098b957154f1b20
lybyerc-lab <lybyerc@gmail.com>
2026-07-31 22:26:43 -0500

    fix: match Capacitor onResume visibility

--- android/app/src/main/java/com/lybyerclab/severeweather/MainActivity.java
+++ android/app/src/main/java/com/lybyerclab/severeweather/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends BridgeActivity {
     }
 
     @Override
-    protected void onResume() {
+    public void onResume() {
         super.onResume();
         enableImmersiveMode();
     }
